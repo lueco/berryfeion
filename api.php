94f001291f7e08a4409ebf4f1f416baaa72b5d01
Lueco Shun <lueco.shun@gmail.com>
2014-04-28 21:24:47 +0800

    Base on FetionPhp http://code.google.com/p/php-fetion/

---
+++ api.php
@@ -0,0 +1,9 @@
+<?php
+require '/lib/PHPFetion.php';
+$phone = $_GET["phone"];
+$pwd = $_GET["pwd"];
+$to = $_GET["to"];
+$msg = $_GET["msg"];
+
+$fetion = new PHPFetion('$phone', '$pwd');	// 手机号、飞信密码
+$fetion->send('$to', '$msg');	// 接收人手机号、飞信内容
